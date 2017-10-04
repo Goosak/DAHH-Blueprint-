d332af3233b6935935a4bceff6b0d031742e4540
Vladimir <azov-97@mail.ru>
2017-10-04 22:12:14 +0700

    Add files via upload

---
+++ DAHH(Blueprint)/dependings.lua
@@ -0,0 +1,14 @@
+depends = {}
+depends.icons = {}
+depends.sprites = {}
+depends.sounds = {}
+depends.music = {}
+
+depends.loadPaths = function()
+depends.icons = "res/icons"
+depends. = "res/sprites"
+strSou = "res/sounds"
+strMus = "res/music"
+end
+
+return depends
